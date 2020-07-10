043983d43aff663db281fe7d1a63890d8968b3c6
RV <reijo.vosu.work@gmail.com>
2020-07-10 20:56:06 +0300

    update sudoku

---
+++ sudoku/precache-manifest.c743a1b6367723ac004d95db9d33e4b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85af4b0c217417d237ec9f5df04abc3c",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "891779a1777f5c66b1f4",
+    "url": "/sudoku/static/css/main.8f6c9c88.chunk.css"
+  },
+  {
+    "revision": "a36d438c6c9bf0147e21",
+    "url": "/sudoku/static/js/2.4b289925.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/sudoku/static/js/2.4b289925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891779a1777f5c66b1f4",
+    "url": "/sudoku/static/js/main.16806799.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  },
+  {
+    "revision": "420a8da6cb432222b2f8c6ad80c23c73",
+    "url": "/sudoku/static/media/close.420a8da6.svg"
+  },
+  {
+    "revision": "619556cf191db550c99551c0b99cf501",
+    "url": "/sudoku/static/media/reload.619556cf.svg"
+  },
+  {
+    "revision": "607d7ae7a61666bf166b9fa13ead5747",
+    "url": "/sudoku/static/media/settings.607d7ae7.svg"
+  }
+]);
